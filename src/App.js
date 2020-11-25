bc09395bb6592bc85943678d0f6b9edf478fe529
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-11-24 22:55:38 -0500

    Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { extend } from 'jquery';
 import './App.css';
+import React from "react";
+import {
+  Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import history from './history.js';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component  {
+
 }
 
 export default App;
